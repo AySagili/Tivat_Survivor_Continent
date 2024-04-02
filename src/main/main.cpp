9c84e3730e138dcaba6723e8d909744e8520bdb9
unknown <AyaSagili>
2024-04-02 23:47:24 +0800

    2024/4/2

---
+++ src/main/main.cpp
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+#include <easyx.h>
+#include <unistd.h>
+#include <iostream>
+#include "board.h"
+#include "Teyvat.h"
+#include "mydb.h"
+
+using namespace std;
+
+int main()
+{
+
+    Teyvat_Run();
+
+    return 0;
+}
